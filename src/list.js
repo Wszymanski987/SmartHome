558a8e3a2da8da5421aba475061cbba0e5f48c5e
Wszymanski987 <85928708+Wszymanski987@users.noreply.github.com>
2022-04-14 19:03:40 +0200

    Aktualizacja projektu

--- src/list.js
+++ src/list.js
@@ -1,9 +1,16 @@
-import react from 'react';
+import react, {useState} from 'react';
+import Button from './components.js/button';
+import SmartDevice from './components.js/smartDevice';
 
 const List = () => {
+    const [clicked, setClicked] = useState(false);
+
     return (
         <li>
-            <ul>SmartBulb</ul>
+            <ul>
+                <Button onClick={() => setClicked(clicked = !clicked)} />
+                 
+            </ul>
             <ul>SmartOutlet</ul>
             <ul>SmartTemperatureSensor</ul>
         </li>
