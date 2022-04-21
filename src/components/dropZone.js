139ec3934a9503eacf24d035cadc457dd3cef277
aserwois <mateusz.gabriel90@gmail.com>
2022-04-21 18:08:58 +0200

    testing branch

--- src/components/dropZone.js
+++ src/components/dropZone.js
@@ -1,44 +1,94 @@
 import interact from "interactjs";
 import styled from "styled-components";
+import "./test.css";
 
 const S = {
-    Wrapper: styled.div`
+  Wrapper: styled.div`
     height: 500px;
     width: 500px;
     background-color: #ffffff;
-    `
-}
+  `,
+};
 
-interact ('div')
-    .dropzone ({
-        accept: '{ item }',
-        overlap: 0.75,
-        ondropactivate: function (event) {
-            const item = event.relatedTarget
-            item.classList.add('dragging')
-          },
-          ondropdeactivate: function (event) {
-            const item = event.relatedTarget
-            item.classList.remove('dragging', 'cannot-drop')
-          },
-          ondragenter: function(event) {
-            const item = event.relatedTarget
-            item.classList.remove('cannot-drop')
-            item.classList.add('can-drop')
-          },
-          ondragleave: function(event) {
-            const item = event.relatedTarget
-            item.classList.remove('can-drop')
-            item.classList.add('cannot-drop')
-          }
-        })
+interact(".dropzone").dropzone({
+  // only accept elements matching this CSS selector
+  accept: "#yes-drop",
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add("drop-active");
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget;
+    var dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add("drop-target");
+    draggableElement.classList.add("can-drop");
+    draggableElement.textContent = "Dragged in";
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove("drop-target");
+    event.relatedTarget.classList.remove("can-drop");
+    event.relatedTarget.textContent = "Dragged out";
+  },
+  ondrop: function (event) {
+    event.relatedTarget.textContent = "Dropped";
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove("drop-active");
+    event.target.classList.remove("drop-target");
+  },
+});
+
+interact(".drag-drop").draggable({
+  inertia: true,
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: "parent",
+      endOnly: true,
+    }),
+  ],
+  autoScroll: true,
+  // dragMoveListener from the dragging demo above
+  // listeners: { move: dragMoveListener },
+});
+
+// interact("div").dropzone({
+//   accept: ".ttt",
+//   overlap: 0.75,
+//   ondropactivate: function (event) {
+//     const item = event.relatedTarget;
+//     item.classList.add("dragging");
+//   },
+//   ondropdeactivate: function (event) {
+//     const item = event.relatedTarget;
+//     item.classList.remove("dragging", "cannot-drop");
+//   },
+//   ondragenter: function (event) {
+//     const item = event.relatedTarget;
+//     item.classList.remove("cannot-drop");
+//     item.classList.add("can-drop");
+//   },
+//   ondragleave: function (event) {
+//     const item = event.relatedTarget;
+//     item.classList.remove("can-drop");
+//     item.classList.add("cannot-drop");
+//   },
+// });
 
 const DropZone = () => {
-    return (
-        <S.Wrapper>
-            <h2>Device Details</h2>
-        </S.Wrapper>
-    );
+  return (
+    <S.Wrapper id="outer-dropzone" class="dropzone">
+      <h2>Device Details</h2>
+    </S.Wrapper>
+  );
 };
 
 export default DropZone;
--- src/list.js
+++ src/list.js
@@ -3,59 +3,57 @@ import interact from "interactjs";
 
 import { DEVICES } from "./mock/devices";
 
-  const S = {
-    StyledList: styled.li`
+const S = {
+  StyledList: styled.li`
     color: black;
     background-color: red;
-    `,
-    StyledItemPosition: styled.div`
+  `,
+  StyledItemPosition: styled.div`
     border: solid black;
     cursor: pointer;
-    tranistion: 1s;
+    transition: 1s;
     :hover {
-        opacity: 0.8;
-    };
-      `   
-  };
+      opacity: 0.8;
+    }
+  `,
+};
 
-  interact('div')
-    .draggable({
-      onmove: function(event) {
-        const target = event.target;
+const test = (event) => {
+  const target = event.target;
 
-        const dataX = target.getAttribute('data-x');
-        const dataY = target.getAttribute('data-y');
-        const initialX = parseFloat(dataX) || 0;
-        const initialY = parseFloat(dataY) || 0;
+  const dataX = target.getAttribute("data-x");
+  const dataY = target.getAttribute("data-y");
+  const initialX = parseFloat(dataX) || 0;
+  const initialY = parseFloat(dataY) || 0;
 
-        const deltaX = event.dx;
-        const deltaY = event.dy;
+  const deltaX = event.dx;
+  const deltaY = event.dy;
 
-        const newX = initialX + deltaX;
-        const newY = initialY + deltaY;
+  const newX = initialX + deltaX;
+  const newY = initialY + deltaY;
 
-        target
-          .style
-          .transform = `translate(${newX}px, ${newY}px)`;
+  target.style.transform = `translate(${newX}px, ${newY}px)`;
 
-        target.setAttribute('data-x', newX);
-        target.setAttribute('data-y', newY);
-      }
-    })
+  target.setAttribute("data-x", newX);
+  target.setAttribute("data-y", newY);
+};
 
+interact(".ttt").draggable({
+  onmove: test,
+});
 
 const Lists = ({ setClickedDevice }) => {
-  return ( 
+  return (
     <S.StyledList>
       {DEVICES.map((item, index) => (
-        <S.StyledItemPosition>
-          <ul onClick={() => setClickedDevice(item)} key={index}>{item.name}</ul>
+        <S.StyledItemPosition id="yes-drop" className="drag-drop">
+          <ul onClick={() => setClickedDevice(item)} key={index}>
+            {item.name}
+          </ul>
         </S.StyledItemPosition>
       ))}
-    </S.StyledList>   
-    
+    </S.StyledList>
   );
 };
 
 export default Lists;
-
